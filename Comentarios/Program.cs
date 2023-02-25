84522532e6ef1e2d3bc2ac50089cef498ae201b9
Luanaap <57630311+Luanaap@users.noreply.github.com>
2023-02-24 23:11:07 -0300

    Criando repositório para Comentarios

---
+++ Comentarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Este comando mostra a mensagem
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadKey();
+        }
+    }
+}
